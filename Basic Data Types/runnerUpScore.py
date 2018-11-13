952bf84daf3baad1eb4810eaf5dc522b7aacf21d
Vladislav Petrishin <petrishin.vladislav@gmail.com>
2018-11-13 21:51:44 +0200

    Basic Data Types - Runner Up Score

---
+++ Basic Data Types/runnerUpScore.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    max_val = max(arr)
+    while(max_val in arr):
+        arr.remove(max_val)
+    print(max(arr))
