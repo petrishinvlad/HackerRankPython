ce58cc1d2e42ff0dd530db2c378931e5d4f76f8e
Vladislav Petrishin <petrishin.vladislav@gmail.com>
2018-11-15 13:48:31 +0200

    Data types - removing comment from student marks task + tuple hash

--- Basic Data Types/studentMarks.py
+++ Basic Data Types/studentMarks.py
@@ -1,12 +1,3 @@
-'''
-
-3
-Krishna 67 68 69
-Arjun 70 98 63
-Malika 52 56 60
-Malika
-
-'''
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
---
+++ Basic Data Types/tupleHash.py
@@ -0,0 +1,5 @@
+#import __builtin__
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    print(hash(tuple(integer_list)))
